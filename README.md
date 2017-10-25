798c358df422e6d48738d5b8ce5e8fd4bac4f1f0
Jason Koh <bk7749@gmail.com>
2017-10-24 22:26:48 -0700

    submodule cleanup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dockerized BuildingDepot.
+
+
+## Running Instruction
+
+### Docker Image Building
+bash configure.sh
+bash build.sh
--- build.sh
+++ build.sh
@@ -4,12 +4,25 @@
 #docker build -t bd3ds .
 #docker push jbkoh/bd3ds
 
+bash configure.sh
+
 rm -r bd3cs/app
 cp -r BuildingDepot-v3/buildingdepot/CentralService bd3cs/app
 cp -r configs bd3cs/
 cp BuildingDepot-v3/pip_packages.list bd3cs/
 cd bd3cs
 docker build -t bd3cs .
+
+rm -r bd3ds/app
+cp -r BuildingDepot-v3/buildingdepot/DataService bd3ds/app
+cp -r configs bd3ds/
+cp BuildingDepot-v3/pip_packages.list bd3ds/
+cd bd3ds
+docker build -t bd3ds .
+
+
+
+
 #docker push jbkoh/bd3cs
 
 #docker pull mongo
