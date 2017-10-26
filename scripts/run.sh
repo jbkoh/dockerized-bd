ea879f93e9296ba1869b8af3e980f9826c75094f
Jason Koh <bk7749@gmail.com>
2017-10-25 23:39:47 -0700

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,5 +5,6 @@ docker run -d --name bd_influxdb -p 8086:8086 influxdb
 docker run -d --name bd_mongo -p 27017:27017 mongo
 
 
-bash ../bd3cs_nossl/cs_run.sh
-bash ../bd3dscr_nossl/ds_run.sh
+docker run -d --name mycs -p 81:80 jbkoh/buildingdepot:cs_nossl
+docker run -d --name myds -p 82:80 -p 8080:8080 jbkoh/buildingdepot:ds_nossl
+
