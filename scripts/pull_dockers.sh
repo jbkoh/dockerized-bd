92dd09ba4d5e235162b44192dd720990db6a3468
Jason Koh <bk7749@gmail.com>
2017-10-25 23:33:53 -0700

    update

--- scripts/pull_dockers.sh
+++ scripts/pull_dockers.sh
@@ -3,4 +3,6 @@
 docker pull influxdb 
 docker pull mongo
 
+docker pull jbkoh/buildingdepot:cs_nossl
+docker pull jbkoh/buildingdepot:ds_nossl
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,4 @@ docker run -d --name bd_mongo -p 27017:27017 mongo
 
 
 bash ../bd3cs_nossl/cs_run.sh
-bash ../bd3ds_nossl/ds_run.sh
-bash ../bd3cr/cr_run.sh
-
-
-#docker pull jbkoh/bd3ds
+bash ../bd3dscr_nossl/ds_run.sh
